971992e93bc76630a070077eac71978382d7a8e3
Habu <habu1010+github@gmail.com>
2024-02-08 22:58:59 +0900

    feat: FPSカウンター

--- index.js
+++ index.js
@@ -18,13 +18,55 @@ const playPauseButton = document.getElementById("play-pause");
 
 const ctx = canvas.getContext("2d");
 
+const fps = new (class {
+  constructor() {
+    this.fps = document.getElementById("fps");
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    // Convert the delta time since the last frame render into a measure
+    // of frames per second.
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    const fps = (1 / delta) * 1000;
+
+    // Save only the latest 100 timings.
+    this.frames.push(fps);
+    while (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    let mean = sum / this.frames.length;
+
+    this.fps.textContent = `
+Frames per Second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+      `.trim();
+  }
+})();
+
 let animationId = null;
 
 const renderLoop = () => {
-  drawGrid();
-  drawCells();
+  fps.render();
 
   universe.tick();
+  drawGrid();
+  drawCells();
 
   animationId = requestAnimationFrame(renderLoop);
 };
